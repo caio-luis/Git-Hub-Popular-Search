822c9468355f1f5982d9105a5e7fd59a4431ff64
Caio Luis <caio.oliveira@truckpad.com.br>
2020-10-03 19:27:28 -0300

    added base structure for modularization

---
+++ buildSrc/src/main/java/versions.kt
@@ -0,0 +1,27 @@
+/**
+ * Created by Caio Luis (@caio.luis) on 03/10/20
+ */
+
+internal object KotlinCoreVersions {
+    const val ktxCoreVersion = "1.3.2"
+    const val kotlinVersion = "1.3.72"
+    const val appCompatVersion = "1.2.0"
+}
+
+internal object BuildVersions {
+    const val androidGradle = "4.0.1"
+}
+
+internal object DesignVersions {
+    const val constraintLayoutVersion = "2.0.1"
+}
+
+internal object TestVersions {
+    const val assertJ = "3.11.1"
+    const val androidxJunit = "1.1.1"
+    const val espresso = "3.2.0"
+    const val androidTestArchCore = "1.0.0"
+    const val androidTextRules = "1.1.0"
+    const val androidTextRunner = "1.1.0"
+    const val coreTesting = "2.1.0"
+}
