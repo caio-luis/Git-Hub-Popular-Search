2adb87a8f865dc9e4fe2a4370589d56c6fe64008
Caio Luis <caioluis40@gmail.com>
2023-01-17 23:20:48 -0300

    improve endless listener to work with flow

--- app/src/main/java/com/caioluis/githubpopular/adapter/EndlessScrollListener.kt
+++ app/src/main/java/com/caioluis/githubpopular/adapter/EndlessScrollListener.kt
@@ -2,11 +2,34 @@ package com.caioluis.githubpopular.adapter
 
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import com.caioluis.data.base.throttleFirst
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.channels.BufferOverflow
+import kotlinx.coroutines.flow.MutableSharedFlow
+import kotlinx.coroutines.launch
 
 const val VISIBLE_THRESHOLD = 5
+const val DEBOUNCE_TIMEOUT_MILLIS = 2500L
 
-abstract class EndlessScrollListener(private val layoutManager: LinearLayoutManager) :
+abstract class EndlessScrollListener(
+    private val layoutManager: LinearLayoutManager,
+    private val coroutineScope: CoroutineScope,
+) :
     RecyclerView.OnScrollListener() {
+
+    private val output = MutableSharedFlow<Unit>(
+        onBufferOverflow = BufferOverflow.DROP_LATEST,
+        extraBufferCapacity = 1
+    )
+
+    fun startListeningToEvents() {
+        coroutineScope.launch {
+            output
+                .throttleFirst(DEBOUNCE_TIMEOUT_MILLIS)
+                .collect { onLoadMoreItems() }
+        }
+    }
+
     override fun onScrolled(recyclerView: RecyclerView, dx: Int, dy: Int) {
         if (dy < 0) return
 
@@ -15,7 +38,7 @@ abstract class EndlessScrollListener(private val layoutManager: LinearLayoutMana
         val firstVisibleItem = layoutManager.findFirstVisibleItemPosition()
 
         if (totalItemCount - visibleItemCount <= firstVisibleItem + VISIBLE_THRESHOLD)
-            onLoadMoreItems()
+            output.tryEmit(Unit)
     }
 
     abstract fun onLoadMoreItems()
